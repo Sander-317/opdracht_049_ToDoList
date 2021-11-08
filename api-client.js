3e7bfdd247a69495908fa1db1a8af7ecaebeb3a3
Sander-317 <sander.van.meerveld@gmail.com>
2021-11-08 16:17:47 +0100

    lets try somsthing new

--- api-client.js
+++ api-client.js
@@ -8,29 +8,37 @@ async function getAllTodos() {
             .then((data) => {
             toTheDom(data)
             toDoCounter(data)
+            
         })
     }
     catch(err){
         console.log(err)
     }
+
 }
    
 async function makeNewTodo(event){
     try{
         event.preventDefault()
         const input = userInput.value
-        const data = {todo: `${input}`, done: "false"}
+        const data = {todo: `${input}`, done: false}
             await fetch(url, {
                 method: "POST",
                 body: JSON.stringify(data),
                 headers: {
                     "Content-Type": "application/json",
+                    
                 },
               })
             }
             catch(err){
                 console.log(err)
             }
+    //  dit heb ik gedaan zodat je niet meer de heletijd op refresh moet drukken
+    //  het rare is dat ik zelf het probleem niet had
+    //  en als ik nu  iets toevoeg of op de checkbox click dan verschijnen alle todos 2x voor een sec ongeveer en dan verdijnt de helft  
+    getAllTodos() 
+    
 }
 
 async function deleteTodo(id){
@@ -42,17 +50,16 @@ async function deleteTodo(id){
     catch(err){
         console.log(err)
     }
+    //  HERHALING VAN COMMENT HIER BOVEN 
+    //  dit heb ik gedaan zodat je niet meer de heletijd op refresh moet drukken
+    //  het rare is dat ik zelf het probleem niet had
+    //  en als ik nu  iets toevoeg of op de checkbox click dan verschijnen alle todos 2x voor een sec ongeveer en dan verdijnt de helft
+    getAllTodos()
+    
 }
 
-async function setTodoToStatus(id, checkbox){
+async function setTodoToStatus(id, data){
     try{
-        let data = {}
-        if (checkbox.checked == false){
-            data = { done: "false"}
-        }
-        else {
-            data = { done: "true"}
-        }
         await fetch(`${url}${id}`,{
             method: "PUT",
             body: JSON.stringify(data),
@@ -64,5 +71,11 @@ async function setTodoToStatus(id, checkbox){
     catch(err){
         console.log(err)
     }
+    //  HERHALING VAN COMMENT HIER BOVEN
+    //  dit heb ik gedaan zodat je niet meer de heletijd op refresh moet drukken
+    //  het rare is dat ik zelf het probleem niet had
+    //  en als ik nu  iets toevoeg of op de checkbox click dan verschijnen alle todos 2x voor een sec ongeveer en dan verdijnt de helft
+    getAllTodos()
+    
 }
 
--- script.js
+++ script.js
@@ -8,21 +8,22 @@ addTaskButton.addEventListener("click",  makeNewTodo)
 
 getAllTodos()
 
+
 function toTheDom(array){
-const newArray = []
- array.map((item) => {
+   array.map((item) => {
      const newLi = document.createElement("li")
      const checkBox = document.createElement("input")
      const todoText = document.createElement("p")
      const trashCan = document.createElement("div")
      checkBox.type = "checkbox"
+     checkBox.classList.add("check_box")
      todoText.id =  `${item._id}`
      todoText.innerHTML = `${item.todo} `
      trashCan.innerHTML = `<i class="fas fa-trash-alt"></i>`
      trashCan.classList.add("trash_can")
      isCheckBoxChecked(checkBox, item, todoText)
      trashCan.addEventListener("click", () => deleteTodo(item._id))
-     checkBox.addEventListener("click", () => setTodoToStatus(item._id, checkBox))
+     checkBox.addEventListener("click", () => setTodoToStatus(item._id, {done: checkBox.checked}))
      newLi.appendChild(checkBox)
      newLi.appendChild(todoText)
      newLi.appendChild(trashCan)
@@ -30,21 +31,24 @@ const newArray = []
     })
 }
 
+// samantha ik heb gedaan wat je had laten zien ik heb alleen 1 vraag bij de if statement geef je alleen checkBox.checked 
+// door. checked de if statement automatische voor true of false?  
 function isCheckBoxChecked(checkBox,  item,todoText){
-    if (item.done == "true"){
-        checkBox.checked = true
-        todoText.classList.add("done")
-        }
-    else {
-        checkBox.checked = false
-        todoText.classList.remove("done")
-         }
+    checkBox.checked = item.done
+    if (checkBox.checked) return todoText.classList.add("done")
+    if (!checkBox.checked) return todoText.classList.remove("done")
 }
 
+// ik heb meteen jou systeem toegepast bij mijn todo counter dank je wel
 function toDoCounter(array){
-    const filteredArray = array.filter(item => item.done === "true" )
+    const filteredArray = array.filter(item => item.done ===  true )
     const total = array.length
     const done = filteredArray.length
     const left = (total - done)
-    toDoCount.innerHTML = ` ${done} down ${left} to go`
+    if (total === 0) return  toDoCount.innerHTML = `There is nothing to do.`
+    if (done === 0 && total === 1 ) return  toDoCount.innerHTML = `You have 1 task todo.`
+    if (done === 0 && total > 1) return toDoCount.innerHTML = `You have ${left} tasks todo.`
+    if (done > 0 && left > 0) return toDoCount.innerHTML = `${done} down ${left} to go.`
+    if (done === 1 && left === 0) return  toDoCount.innerHTML = `Mission complete task done`
+    if (done > 0 && left === 0) return toDoCount.innerHTML = `Mission Complete all tasks done.`
 }
